1ca56d70c35c87c6b01aa05ff2e7bb7366558097
joankirui <joankirui99@gmail.com>
2019-07-13 18:05:15 +0300

    add an option button

---
+++ js/scripts.js
@@ -0,0 +1,87 @@
+function name = 
+
+
+
+
+
+
+
+
+
+if((d<=0)||(d>30))
+{
+    alert("Invalid day!!Enter a valid day");
+}
+else{
+    alert("Continue");
+}
+if((m<=0)||(m>12))
+{
+    alert("Invalid month!!Enter a valid month");
+}
+else
+{
+    alert("Continue");
+}
+if(gender===Male)
+{
+    if(d===0)
+    {
+        alert("Your name is Kwasi");
+    }
+    else if(d===1)
+    {
+        alert("Your name is Kwadwo"); 
+    }
+    else if(d===2)
+    {
+        alert("Your name is Kwabena"); 
+    }
+    else if(d===3)
+    {
+        alert("Your name is Kwaku"); 
+    }
+    else if(d===4)
+    {
+        alert("Your name is Yaw"); 
+    }
+    else if(d===5)
+    {
+        alert("Your name is Kofi"); 
+    }
+    else if(d===6)
+    {
+        alert("Your name is Kwame"); 
+    }
+}
+else
+{
+    if(d===0)
+    {
+        alert("Your name is Akoswa");
+    }
+    else if(d===1)
+    {
+        alert("Your name is Adwoa"); 
+    }
+    else if(d===2)
+    {
+        alert("Your name is Abenaa"); 
+    }
+    else if(d===3)
+    {
+        alert("Your name is Akua"); 
+    }
+    else if(d===4)
+    {
+        alert("Your name is Yaa"); 
+    }
+    else if(d===5)
+    {
+        alert("Your name is Afua"); 
+    }
+    else if(d===6)
+    {
+        alert("Your name is Ama"); 
+    }
+}
