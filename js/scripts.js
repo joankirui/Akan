92619b47454a93a8db9b918ede7da57a95ff2762
joankirui <joankirui99@gmail.com>
2019-07-13 19:35:42 +0300

    add a function named akanName

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,12 @@
 function akanName(){
     return d = (((CC/4)-2*CC-1)+((5*YY/4))+((26*(MM+1)/10))+DD)mod 7
 }
-var CC = document.getElementById
+var CC = document.getElementsByName(century);
+var DD= document.getElementsByName(day);
+var MM = document.getElementsByName(month);
+var YY = document.getElementsByName(year);
+
+
 
 
 
@@ -29,61 +34,61 @@ if(gender===Male)
 {
     if(d===0)
     {
-        alert("Your name is Kwasi");
+        alert("Your Akan name is Kwasi");
     }
     else if(d===1)
     {
-        alert("Your name is Kwadwo"); 
+        alert("Your Akan name is Kwadwo"); 
     }
     else if(d===2)
     {
-        alert("Your name is Kwabena"); 
+        alert("Your Akan name is Kwabena"); 
     }
     else if(d===3)
     {
-        alert("Your name is Kwaku"); 
+        alert("Your Akan name is Kwaku"); 
     }
     else if(d===4)
     {
-        alert("Your name is Yaw"); 
+        alert("Your Akan name is Yaw"); 
     }
     else if(d===5)
     {
-        alert("Your name is Kofi"); 
+        alert("Your Akan name is Kofi"); 
     }
     else if(d===6)
     {
-        alert("Your name is Kwame"); 
+        alert("Your Akan name is Kwame"); 
     }
 }
 else
 {
     if(d===0)
     {
-        alert("Your name is Akoswa");
+        alert("Your Akan name is Akoswa");
     }
     else if(d===1)
     {
-        alert("Your name is Adwoa"); 
+        alert("Your Akan name is Adwoa"); 
     }
     else if(d===2)
     {
-        alert("Your name is Abenaa"); 
+        alert("Your Akan name is Abenaa"); 
     }
     else if(d===3)
     {
-        alert("Your name is Akua"); 
+        alert("Your Akan name is Akua"); 
     }
     else if(d===4)
     {
-        alert("Your name is Yaa"); 
+        alert("Your Akan name is Yaa"); 
     }
     else if(d===5)
     {
-        alert("Your name is Afua"); 
+        alert("Your Akan name is Afua"); 
     }
     else if(d===6)
     {
-        alert("Your name is Ama"); 
+        alert("Your Akan name is Ama"); 
     }
 }
