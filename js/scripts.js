9f8d57b5a5f0c295b351814089ba6171096c79c7
joankirui <joankirui99@gmail.com>
2019-07-14 13:00:43 +0300

    add a new branch

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,11 @@ function akanName(){
     return d = (((CC/4)-2*CC-1)+((5*YY/4))+((26*(MM+1)/10))+DD)mod 7;
 }
 var CC = document.getElementsByName(century);
-var DD= document.getElementsByName(day);
+var DD = document.getElementsByName(day);
 var MM = document.getElementsByName(month);
 var YY = document.getElementsByName(year);
-var gender = document.getElementsByName(gender);
-var d =["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 akanName(CC,DD,MM,YY);
+
 if((d<=0)||(d>30))
 {
     alert("Invalid day!!Enter a valid day");
@@ -23,65 +22,62 @@ else
 {
     alert("Continue");
 }
-if(gender===Male)
+var gender = document.getElementsByName(gender);
+var days =["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+if((gender===Male)&&(days===[0]))
 {
-    if(d===[0])
-    {
-        alert("Your Akan name is Kwasi");
-    }
-    else if(d===[1])
-    {
-        alert("Your Akan name is Kwadwo"); 
-    }
-    else if(d===[2])
-    {
-        alert("Your Akan name is Kwabena"); 
-    }
-    else if(d===[3])
-    {
-        alert("Your Akan name is Kwaku"); 
-    }
-    else if(d===[4])
-    {
-        alert("Your Akan name is Yaw"); 
-    }
-    else if(d===5)
-    {
-        alert("Your Akan name is Kofi"); 
-    }
-    else if(d===6)
-    {
-        alert("Your Akan name is Kwame"); 
-    }
+    alert("Your Akan name is Kwasi");
 }
-else
+else if((gender===Male)&&(days===[1])
 {
-    if(d===0)
-    {
-        alert("Your Akan name is Akoswa");
-    }
-    else if(d===1)
-    {
-        alert("Your Akan name is Adwoa"); 
-    }
-    else if(d===2)
-    {
-        alert("Your Akan name is Abenaa"); 
-    }
-    else if(d===3)
-    {
-        alert("Your Akan name is Akua"); 
-    }
-    else if(d===4)
-    {
-        alert("Your Akan name is Yaa"); 
-    }
-    else if(d===5)
-    {
-        alert("Your Akan name is Afua"); 
-    }
-    else if(d===6)
-    {
-        alert("Your Akan name is Ama"); 
-    }
+    alert("Your Akan name is Kwadwo"); 
 }
+else if((gender===Male)&&(days===[2]))
+{
+    alert("Your Akan name is Kwabena"); 
+}
+else if((gender===Male)&&(days===[3]))
+{
+    alert("Your Akan name is Kwaku"); 
+}
+else if((gender===Male)&&(days===[4]))
+{
+    alert("Your Akan name is Yaw"); 
+}
+else if((gender===Male)&&(days===[5]))
+{
+    alert("Your Akan name is Kofi"); 
+}
+else if((gender===Male)&&(days===[6]))
+{
+    alert("Your Akan name is Kwame"); 
+}
+else if((gender===Female)&&(days===[0]))
+{
+    alert("Your Akan name is Akoswa");
+}
+else if((gender===Female)&&(days===[1]))
+{
+    alert("Your Akan name is Adwoa"); 
+}
+else if((gender===Female)&&(days===[2]))
+{
+    alert("Your Akan name is Abenaa"); 
+}
+else if((gender===Female)&&(days===[3]))
+{
+    alert("Your Akan name is Akua"); 
+}
+else if((gender===Female)&&(days===[4]))
+{
+    alert("Your Akan name is Yaa"); 
+}
+else if((gender===Female)&&(days===[5]))
+{
+    alert("Your Akan name is Afua"); 
+}
+else if((gender===Female)&&(days===[6]))
+{
+    alert("Your Akan name is Ama"); 
+}
+
