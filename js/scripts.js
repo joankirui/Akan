55caf50753b46b886140a3b18e2a09d6e26f3851
joankirui <joankirui99@gmail.com>
2019-07-15 11:30:45 +0300

    update javascript code

--- js/scripts.js
+++ js/scripts.js
@@ -1,86 +1,83 @@
-var weekDays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-var femaleNames = ["Akoswa","Adwa","Abena","Akwa","Yaa","Afua","Ama"];
-var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+// var weekDays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+// var femaleNames = ["Akoswa","Adwa","Abena","Akwa","Yaa","Afua","Ama"];
+// var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
 
-function akanName(){
-    var CC = document.getElementsByName(century);
-    var DD = document.getElementsByName(day);
-    if((DD<=0)||(DD>30))
-    {
-        alert("Invalid day!!Enter a valid day");
-    }
-    else
-    {
-        alert("Continue");
-    }
-    var MM = document.getElementsByName(month);
-    if((MM<=0)||(MM>12))
-    {
-        alert("Invalid month!!Enter a valid month");
-    }
-    else
-    {
-        alert("Continue");
-    }
-
-    var YY = document.getElementsByName(year);
-    return d = (((CC/4)-2*CC-1)+((5*YY/4))+((26*(MM+1)/10))+DD)%7;
+function akanday(cc,yy,dd,mm){
+    return ((((cc/4)-2*cc-1) + ((5*yy/4)) + ((26*(mm+1)/10)) + dd)%7);
 }
-var gender = document.getElementsByName(gender);
-if((gender===Male)&&(d===0))
-{
+
+function akanName(form){
+    var cc = document.forms["myform"]["century"].value;
+    var dd = document.forms["myform"]["day"].value;
+    var mm = document.forms["myform"]["month"].value;
+    var yy = document.forms["myform"]["year"].value;
+    var gender = document.forms["myform"]["gender"].value;
+
+    // if((dd<=0)||(dd>31))
+    // {
+    //     alert("Invalid day!!Enter a valid day");
+    // }
+    // else
+    // {
+    //     alert("Continue");
+    // }
+    
+    // if((mm<=0)||(mm>12))
+    // {
+    //     alert("Invalid month!!Enter a valid month");
+    // }
+    // else
+    // {
+    //     alert("Continue");
+    // }
+
+var d= akanday(cc,yy,dd,mm).toFixed();
+// alert("d");
+if(gender== "Male" && d==0){
     alert("Your Akan name is Kwasi");
 }
-else if((gender===Male)&&(d===1)
-{
+else if(gender=="Male" && d==1){
     alert("Your Akan name is Kwadwo"); 
 }
-else if((gender===Male)&&(d===2))
-{
+else if(gender=="Male" && d==2){
     alert("Your Akan name is Kwabena"); 
 }
-else if((gender===Male)&&(d===3))
-{
+else if(gender=="Male" && d==3){
     alert("Your Akan name is Kwaku"); 
 }
-else if((gender===Male)&&(d===4))
-{
+else if(gender=="Male" && d==4){
     alert("Your Akan name is Yaw"); 
 }
-else if((gender===Male)&&(d===5))
-{
+else if(gender=="Male" && d==5){
     alert("Your Akan name is Kofi"); 
 }
-else if((gender===Male)&&(d===6))
+else if(gender== "Male" && d==6)
 {
     alert("Your Akan name is Kwame"); 
 }
-else if((gender===Female)&&(d===0))
-{
-    alert("Your Akan name is Akoswa");
+else if(gender=="Female" && d==0){
+    alert("Your Akan name is Akosua");
 }
-else if((gender===Female)&&(d===1))
-{
+else if(gender=="Female" && d==1){
     alert("Your Akan name is Adwoa"); 
 }
-else if((gender===Female)&&(d===2))
-{
+else if(gender=="Female" && d==2){
     alert("Your Akan name is Abenaa"); 
 }
-else if((gender===Female)&&(d===3))
-{
+else if(gender=="Female" && d==3){
     alert("Your Akan name is Akua"); 
 }
-else if((gender===Female)&&(d===4))
-{
+else if(gender=="Female" && d==4){
     alert("Your Akan name is Yaa"); 
 }
-else if((gender===Female)&&(d===5))
-{
+else if(gender=="Female" && d==5){
     alert("Your Akan name is Afua"); 
 }
-else if((gender===Female)&&(d===6))
-{
+else if (gender=="Female" && d==6){
     alert("Your Akan name is Ama"); 
 }
+else {
+    alert("null");
+}
 
+}
